b01a00cea6396b844ebd98c9ddfc4913ee560be1
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-08-01 02:08:29 -0300

    update

--- src/public/js/indexProducts.js
+++ src/public/js/indexProducts.js
@@ -65,6 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(data)
                 })
+                
+                alert("Se modifico el producto")
                 document.getElementById('formMP').reset()
             } catch (error) {
                 console.error('Error:', error)
